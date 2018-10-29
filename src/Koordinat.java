7d4f5b7ee831bcfc4c8c7d0d5c6520f1cc8f6921
FirmanArdhiansyah <firmanardhi14@gmail.com>
2018-10-29 16:00:45 +0700

    Latihan54

---
+++ src/Koordinat.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nama : Firman Ardhiansyah
+ *         Kelas : IF-2 (PB02)
+ *         NIM : 10117056
+ * Deskripsi Program : program tentang koordinat
+ */
+public class Koordinat {
+
+    protected int x;
+    protected int y;
+
+    public Koordinat(int x, int y) {
+        setX(x);
+        setY(y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
+}
---
+++ src/Warnakoordinat.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Firman
+ */
+public class Warnakoordinat extends Koordinat {
+    public String namaWarna;
+
+    public Warnakoordinat(int x, int y, String namaWarna) {
+        super(x,y);
+        setNamaWarna(namaWarna);
+    }
+
+    public String getNamaWarna() {
+        return namaWarna;
+    }
+
+    public void setNamaWarna(String namaWarna) {
+        this.namaWarna = namaWarna;
+    }
+
+}
+    
